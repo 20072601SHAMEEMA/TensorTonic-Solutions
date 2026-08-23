0ba6226f2655ade697893cc4971b956401a73a80
20072601SHAMEEMA <shameema2114@gmail.com>
2026-08-23 15:00:36 +0530

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def sigmoid(x):
+def sigmoid(x: list | float) -> np.ndarray | float:
     """
     Vectorized sigmoid function.
     """
     # Write code here
     x=np.asarray(x,dtype=float)
     sig=1/(1+np.exp(-x))
-    return sig
+    return float(sig) if sig.ndim==0 else sig
     pass
